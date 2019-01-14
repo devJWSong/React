c2d2101d40c85df02cd7f2e547273c09ca2f93fa
devJWSong <enflwodn@gmail.com>
2019-01-15 01:06:58 +0900

    Used ref

--- hello-react/src/App.js
+++ hello-react/src/App.js
@@ -1,10 +1,15 @@
 import React, { Component } from 'react';
-import EventPractice from './EventPractice'
+import ScrollBox from './ScrollBox';
 
 class App extends Component {
   render() {
     return (
-      <EventPractice/>  
+      <div>
+        <ScrollBox ref={(ref) => this.scrollBox=ref}/>
+        <button onClick={() => this.scrollBox.scrollToBottom()}>
+          To Bottom
+        </button>  
+      </div>
     );
   }
 }
