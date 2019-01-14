b78812c95c12faf4305629de9350f1506bff88c2
devJWSong <enflwodn@gmail.com>
2019-01-14 22:12:33 +0900

    Component promp and state

--- hello-react/src/App.js
+++ hello-react/src/App.js
@@ -1,35 +1,10 @@
 import React, { Component } from 'react';
-import './App.css';
+import MyComponent from './MyComponent';
 
 class App extends Component {
   render() {
-    const text = "Are you awesome?";
-    const condition = true;
-    const style = {
-      backgroundColor: 'gray',
-      border: '1px solid black',
-      height: Math.round(Math.random() * 300) + 50,
-      width: Math.round(Math.random() * 300) + 50,
-      WebkitTransition: 'all',
-      MozTransition: 'all',
-      msTransition: 'all'
-    }
-
     return (
-      <div className = 'my-div'>
-        {/* Comment 1 */}
-        <h1>Hi, React!</h1>
-        <h2>{text}</h2>
-        {condition && "Show me"}
-        <div 
-          style={style}
-          // only working in self-closed tag
-          // /> must be in the next line
-          /* also availiable */
-        />
-        // This will be rendered as it is.
-        /* A comment cannot be written here. */
-      </div>
+      <MyComponent name="React" age={4}/>  
     );
   }
 }
