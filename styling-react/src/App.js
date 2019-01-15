8e2e1fe48fb9a3620e372a7b55742c1a898f9538
devJWSong <enflwodn@gmail.com>
2019-01-15 18:18:39 +0900

    Tried various styling

--- styling-react/src/App.js
+++ styling-react/src/App.js
@@ -1,9 +1,9 @@
 import React, { Component } from 'react';
-import SassComponent from './SassComponent'
+import StyledComponent from './StyledComponent'
 
 class App extends Component {
   render() {
-    return <SassComponent />;
+    return <StyledComponent />;
   }
 }
 
---
+++ styling-react/src/CSSModule.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import classNames from 'classnames/bind'
+import styles from './CSSModule.module.scss'
+
+const cx = classNames.bind(styles)
+
+const CSSModule = () => {
+    return (
+        <div className={cx('wrapper', 'inverted')}>
+            Hi, I'm <span className="something">CSS Module</span>
+        </div>
+    );
+}
+
+export default CSSModule;
