d02fc9fe80b4b7a07b915d974a143646651cc18e
devJWSong <enflwodn@gmail.com>
2019-01-25 23:23:56 +0900

    MongoDB setting

--- blog/blog-backend/src/index.js
+++ blog/blog-backend/src/index.js
@@ -1,3 +1,19 @@
+require('dotenv').config();
+
+const mongoose = require('mongoose');
+
+const {
+  PORT: port = 4000,
+  MONGO_URI: mongoURI
+} = process.env;
+
+mongoose.Promise = global.Promise;
+mongoose.connect(mongoURI).then(() => {
+  console.log('connected to mongodb');
+}).catch((e) => {
+  console.error(e);
+});
+
 const Koa = require('koa');
 const Router = require('koa-router');
 const bodyParser = require('koa-bodyparser');
@@ -13,6 +29,6 @@ app.use(bodyParser());
 
 app.use(router.routes()).use(router.allowedMethods());
 
-app.listen(4000, () => {
-  console.log('listening to port 4000');
+app.listen(port, () => {
+  console.log('listening to port ', port);
 });
