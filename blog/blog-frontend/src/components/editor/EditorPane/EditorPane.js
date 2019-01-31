18e350077a1f0b7e81a5a70b5df070ede8030811
devJWSong <enflwodn@gmail.com>
2019-01-31 18:23:47 +0900

    Postpage and Editor

---
+++ blog/blog-frontend/src/components/editor/EditorPane/EditorPane.js
@@ -0,0 +1,50 @@
+import React, { Component } from 'react';
+import styles from './EditorPane.scss';
+import classNames from 'classnames/bind';
+
+import CodeMirror from 'codemirror';
+
+import 'codemirror/mode/markdown/markdown';
+import 'codemirror/mode/javascript/javascript';
+import 'codemirror/mode/jsx/jsx';
+import 'codemirror/mode/css/css';
+import 'codemirror/mode/shell/shell';
+
+import 'codemirror/lib/codemirror.css';
+import 'codemirror/theme/monokai.css';
+
+const cx = classNames.bind(styles);
+
+class EditorPane extends Component {
+
+  editor = null;
+  codeMirror = null;
+
+  initializeEditor = () => {
+    this.codeMirror = CodeMirror(this.editor, {
+      mode: 'markdown',
+      theme: 'monokai',
+      lineNumbers: true,
+      lineWrapping: true
+    });
+  }
+
+  componentDidMount() {
+    this.initializeEditor();
+  }
+
+  render() {
+    return (
+      <div className={cx('editor-pane')}>
+        <input className={cx('title')} placeholder="Type the title"/>
+        <div className={cx('code-editor')} ref={ref => this.editor=ref}></div>
+        <div className={cx('tags')}>
+          <div className={cx('description')}>Tags</div>
+          <input name="tags" placeholder="Type tags (separate by comma)"/>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default EditorPane;
